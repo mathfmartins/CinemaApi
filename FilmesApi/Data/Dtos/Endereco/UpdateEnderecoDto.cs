14efd8fbedbe17b93fb73e82df1e782c4de6b9d8
Matheus Martins <math.fmartins42@gmail.com>
2022-01-14 16:55:09 -0300

    Endereco -> Model, Dto e Profile incluido.

---
+++ FilmesApi/Data/Dtos/Endereco/UpdateEnderecoDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Data.Dtos.Endereco
+{
+    public class UpdateEnderecoDto
+    {
+        [Required(ErrorMessage = "O campo logradouro é obrigatório.")]
+        public string Logradouro { get; set; }
+        [Required(ErrorMessage = "O campo numero é obrigatório.")]
+        public int Numero { get; set; }
+        [Required(ErrorMessage = "O campo bairro é obrigatório")]
+        public string Bairro { get; set; }
+    }
+}
---
+++ FilmesApi/Models/Endereco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesApi.Models
+{
+    public class Endereco
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo logradouro é obrigatório.")]
+        public string Logradouro { get; set; }
+        [Required(ErrorMessage = "O campo numero é obrigatório.")]
+        public int Numero { get; set; }
+        [Required(ErrorMessage = "O campo bairro é obrigatório")]
+        public string Bairro { get; set; }
+    }
+}
